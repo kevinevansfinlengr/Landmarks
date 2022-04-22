017688e3789831dfccf787d84e9f86812bfe9ea9
Kevin Evans <kevinevansfinlengr@gmail.com>
2022-04-22 15:15:56 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by KEVIN EVANS on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
