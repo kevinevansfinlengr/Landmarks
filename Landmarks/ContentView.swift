37a54e8b0c5f3cef1b212a39350ac2979330ceeb
Kevin Evans <kevinevansfinlengr@gmail.com>
2022-04-22 16:14:41 -0400

    add CircleImage View file to format pictures... add one picture of Turtle Rock

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
-            .foregroundColor(.black)
-            .padding()
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+                .foregroundColor(.black)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.headline)
+            }
+        }.padding()
     }
 }
 
