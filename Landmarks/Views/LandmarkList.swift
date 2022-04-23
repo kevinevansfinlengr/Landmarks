6c6694d89ce3b313cc81052cff66fcf827eee49d
Kevin Evans <kevinevansfinlengr@gmail.com>
2022-04-23 00:07:34 -0400

    add LandmarkListView and make the Landmark model Identifiable

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by KEVIN EVANS on 4/23/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
